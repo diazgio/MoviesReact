89333a4c4a6ecc12b4432ba654db163c08f8dd40
Giordano Dìaz <diazgiordano@gmail.com>
2021-11-07 16:30:22 -0500

    Finish the pagination part

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
+import { useState } from 'react';
 import PageWrapper from './PageWrapper';
 import Pelicula from './pelicula';
-import peliculasJsom from './peliculas.json';
+import peliculasJson from './peliculas.json';
 import Pagination from './pagination';
 
 function App() {
-  let peliculas = peliculasJsom;
+
+  const [actualPage, setActualPage] = useState(1);
+  const TOTAL_POR_PAGINA = 5;
+  let peliculas = peliculasJson;
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice(
+      (actualPage - 1) * TOTAL_POR_PAGINA,
+      actualPage * TOTAL_POR_PAGINA
+    );
+  }
+
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = peliculasJson.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  cargarPeliculas();
   
   return (
     <PageWrapper>
@@ -23,12 +41,13 @@ function App() {
           director2={p.director2}
           actor1={p.actor1}
           actor2={p.actor2}
-          actor3={p.actor3}>
+          actor3={p.actor3}
+          key={p.title}>
           {p.describe}
         </Pelicula>
       )}
-      <Pagination page={2} total={4} onChange={(page) => {
-
+      <Pagination page={actualPage} total={getTotalPaginas()} onChange={(page) => {
+        setActualPage(page)
       }}/>
 
     </PageWrapper>
--- src/pagination.js
+++ src/pagination.js
@@ -1,31 +1,29 @@
+/* eslint-disable eqeqeq */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 export default function Pagination(props) {
 
-  const getPage = () => {
+  const getActulPage = () => {
     const result = [];
-    for(let i; i < props.total; i++) {
-      result.push(
-      <>
-        <a onClick={props.onChange}
-          className={props.page == i ? "active" : ""}
-          href="#">
-          {(i + 1)}
-        </a>
-        <a href="#"><i className="ion-arrow-right-b"></i></a>
-      </>
-      );
 
+    for (let i = 0; i < props.total; i++) {
+      let pagina = i + 1;
+      result.push(
+        <a key={i} onClick={() => props.onChange(pagina)}
+          className={props.page === pagina ? 'active' : ''}>
+          {pagina}
+        </a>
+      );
     }
+
     return result;
   }
+
   return(
     <div className="topbar-filter">
       
       <div className="pagination2">
         <span>Page {props.page} of {props.total}:</span>
-        {getPage()}
-        <a href="#">2</a>
-        
+        {getActulPage()}
       </div>
     </div>
   );
